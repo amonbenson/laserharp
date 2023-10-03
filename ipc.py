6b45ca0eb1c7ae126069e6efd8ad042671056c68
Amon Benson <amon.k.benson@campus.tu-berlin.de>
2023-10-03 16:52:45 +0200

    finished implementation of IPC

--- ipc.py
+++ ipc.py
@@ -1,73 +1,77 @@
 from enum import Enum
+from serial import Serial
 import mido
 
 
-class Packet:
+class IPCPacket:
+    class Command(Enum):
+        MIDI_DIN = 0x00
+        MIDI_USB = 0x01
+        MIDI_BLE = 0x02
+
+        SET_LASER_SINGLE = 0x10
+        SET_LASER_EACH = 0x11
+        SET_LASER_ALL = 0x12
+
+
     def __init__(self, cmd, data = b''):
-        self.cmd = int(cmd)
+        self.cmd = IPCPacket.Command(cmd)
         self.data = bytearray(data)
 
-    def bytes(self):
-        header = bytearray([self.cmd, len(self.data)])
-        return header + self.data
+    @property
+    def header(self) -> bytearray:
+        return bytearray([self.cmd.value, len(self.data)])
 
-    """
-    @staticmethod
-    def from_bytes(self, bytes: bytearray):
-        if len(bytes) < 2:
-            raise ValueError("packet must be at least 2 bytes long")
-
-        cmd = bytes[0]
-        length = bytes[1]
-
-        if len(bytes) != length + 2:
-            raise ValueError(f"packet length mismatch: expected {length + 2}, got {len(bytes)}")
-
-        return Packet(cmd, bytes[2:])
-    """
-
-
-class MidiPacket(Packet):
-    class Interface(Enum):
-        DIN = 0x00
-        USB = 0x01
-        BLE = 0x02
-
-    def __init__(self, interface_id: int, data: bytearray = b''):
-        cmd = 0x00 | interface_id & 0x0f
-        super().__init__(cmd, data)
+    def bytes(self) -> bytearray:
+        return self.header + self.data
 
     @staticmethod
-    def from_message(interface: Interface, message: mido.Message):
-        return MidiPacket(interface.value, message.bytes())
+    def from_bytes(data: bytearray):
+        if len(data) < 2:
+            raise ValueError("Packet too short")
 
+        cmd = data[0]
+        length = data[1]
+        data = data[2:]
 
-class LaserPacket(Packet):
-    class Command(Enum):
-        SET_SINGLE = 0x00
-        SET_ALL = 0x01
-        SET_EACH = 0x02
+        if len(data) != length:
+            raise ValueError(f"Packet length mismatch: expected {length}, got {len(data)}")
 
-    def __init__(self, cmd: Command, data: bytearray = b''):
-        cmd = 0x01 | cmd & 0x0f
-        super().__init__(cmd, data)
-
-    @staticmethod
-    def set_single(index: int, brightness: int):
-        return LaserPacket(LaserPacket.Command.SET_SINGLE.value, [index, brightness])
-
-    @staticmethod
-    def set_all(brightness: int):
-        return LaserPacket(LaserPacket.Command.SET_ALL.value, [brightness])
-
-    @staticmethod
-    def set_each(brightnesses: bytearray):
-        return LaserPacket(LaserPacket.Command.SET_EACH.value, brightnesses)
+        return IPCPacket(IPCPacket.Command(cmd), data)
 
 
 class IPCConnector():
-    def __init__(self):
-        pass
+    class MidiInterface(Enum):
+        DIN = IPCPacket.Command.MIDI_DIN.value
+        USB = IPCPacket.Command.MIDI_USB.value
+        BLE = IPCPacket.Command.MIDI_BLE.value
 
-    def send(self, cmd: int, data: bytearray):
-        header = bytearray([cmd, len(data)])
+    def __init__(self, port: str, baudrate: int = 115200, custom_serial = None):
+        if custom_serial is not None:
+            self._serial = custom_serial
+        else:
+            self._serial = Serial(port, baudrate)
+
+        self.rx_packets = []
+
+    def _send(self, packet: IPCPacket):
+        self._serial.write(packet.bytes())
+
+    def available(self) -> bool:
+        return self._serial.in_waiting >= 2
+
+    def read(self) -> IPCPacket:
+        # TODO: might miss packets if they arrive too fast
+        return IPCPacket.from_bytes(self._serial.read_all())
+
+    def send_midi(self, interface: MidiInterface, message: mido.Message):
+        self._send(IPCPacket(interface.value, message.bytes()))
+
+    def set_laser(self, index: int, value: int):
+        self._send(IPCPacket(IPCPacket.Command.SET_LASER_SINGLE, bytearray([index, value])))
+
+    def set_each_laser(self, values: list):
+        self._send(IPCPacket(IPCPacket.Command.SET_LASER_EACH, bytearray(values)))
+
+    def set_all_lasers(self, value: int):
+        self._send(IPCPacket(IPCPacket.Command.SET_LASER_ALL, bytearray([value])))
--- tests/test_ipc.py
+++ tests/test_ipc.py
@@ -1,28 +1,63 @@
 import unittest
 import mido
-from ..ipc import Packet, MidiPacket, LaserPacket
+from ..ipc import IPCConnector
+
+class MockSerial:
+    def __init__(self):
+        self.txdata = bytearray()
+        self.rxdata = bytearray()
+
+    def clear(self):
+        self.txdata = bytearray()
+        self.rxdata = bytearray()
+
+    @property
+    def in_waiting(self):
+        return len(self.rxdata)
+
+    def read(self, n):
+        data = self.rxdata[:n]
+        self.rxdata = self.rxdata[n:]
+        return data
+
+    def read_all(self):
+        data = self.rxdata
+        self.rxdata = bytearray()
+        return data
+
+    def write(self, data):
+        self.txdata += data
 
 
-class Test_Packet(unittest.TestCase):
-    def test_empty_packet(self):
-        packet = Packet(0x00)
-        self.assertEqual(packet.bytes(), b'\x00\x00')
+class Test_IPCConnector(unittest.TestCase):
+    def setUp(self):
+        self.serial = MockSerial()
+        self.conn = IPCConnector('', custom_serial=self.serial)
 
-    def test_midi_packet(self):
-        packet = MidiPacket.from_message(MidiPacket.Interface.USB, mido.Message('note_on', note=0x3c, velocity=0x40))
-        self.assertEqual(packet.bytes(), b'\x01\x03\x90\x3c\x40')
+    def tearDown(self):
+        self.serial.clear()
 
-    """
-    def test_laser_packet(self):
-        packet = LaserPacket.set_single(0x01, 0x80)
-        self.assertEqual(packet.bytes(), b'\x10\x02\x01\x80')
+    def test_send_midi(self):
+        self.conn.send_midi(IPCConnector.MidiInterface.USB, mido.Message('note_on', note=60, velocity=64))
+        self.assertEqual(self.serial.txdata, bytearray([0x01, 3, 0x90, 60, 64]))
 
-        packet = LaserPacket.set_all(0x80)
-        self.assertEqual(packet.bytes(), b'\x11\x01\x80')
+    def test_set_laser(self):
+        self.conn.set_laser(5, 234)
+        self.assertEqual(self.serial.txdata, bytearray([0x10, 2, 5, 234]))
 
-        packet = LaserPacket.set_each([0x80, 0x40, 0x20, 0x10])
-        self.assertEqual(packet.bytes(), b'\x12\x04\x80\x40\x20\x10')
-    """
+    def test_set_each_laser(self):
+        self.conn.set_each_laser([0, 255, 127, 63])
+        self.assertEqual(self.serial.txdata, bytearray([0x11, 4, 0, 255, 127, 63]))
+
+    def test_set_all_lasers(self):
+        self.conn.set_all_lasers(123)
+        self.assertEqual(self.serial.txdata, bytearray([0x12, 1, 123]))
+
+    def test_read(self):
+        self.serial.rxdata = bytearray([0x01, 3, 0x90, 60, 64])
+        packet = self.conn.read()
+        self.assertEqual(packet.cmd.value, 0x01)
+        self.assertEqual(packet.data, bytearray([0x90, 60, 64]))
 
 
 if __name__ == '__main__':
