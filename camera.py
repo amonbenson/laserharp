1cfddbc38ef610348dfd50715ecd8b53c75785ce
Amon Benson <amon.k.benson@campus.tu-berlin.de>
2023-10-04 19:37:07 +0200

    added better camera processing

--- camera.py
+++ camera.py
@@ -1,55 +1,78 @@
+import time
 import threading
 import picamera
 import numpy as np
 
 
 class Camera:
+    class ImageProcessor:
+        def __init__(self, resolution):
+            self.resolution = resolution
+
+            self.frame = None
+            self.frame_lock = threading.Lock()
+            self.frame_event = threading.Event()
+
+        def write(self, buf):
+            w, h = self.resolution
+
+            # get a grayscale frame
+            with self.frame_lock:
+                # convert the YUV stream to a numpy array and extract the Y component
+                self.frame = np.frombuffer(buf, dtype=np.uint8).reshape((h * 3 // 2, w))
+                self.frame = self.frame[:h, :w]
+
+                self.frame_event.set()
+
+            # TODO: image processing
+
+        def flush(self):
+            # clear the event
+            self.frame_event.clear()
+
+        def get_frame_safe(self):
+            # wait for a frame to be available
+            self.frame_event.wait()
+
+            # return a copy of the current frame
+            with self.frame_lock:
+                return self.frame.copy()
+
     def __init__(self):
         self.camera = picamera.PiCamera()
-        self.camera.resolution = (320, 240) # QVGA resolution
+        self.camera.resolution = (640, 480) # VGA resolution
         self.camera.framerate = 60
         self.camera.rotation = 180
         self.thread = None
         self.running = False
 
+        self.image_processor = self.ImageProcessor(resolution=self.camera.resolution)
+
     def start(self):
-        if not self.running:
-            self.thread = threading.Thread(target=self._capture_loop)
-            self.thread.start()
-            self.running = True
+        if self.running: return
+
+        self.thread = threading.Thread(target=self._capture_loop)
+        self.thread.start()
+        self.running = True
 
     def stop(self):
-        if self.running:
-            self.running = False
-            self.thread.join()
+        if not self.running: return
+
+        self.running = False
+        self.thread.join()
 
     def _capture_loop(self):
-        stream = picamera.PiCameraCircularIO(self.camera, seconds=10)
-        self.camera.start_recording(stream, format='h264')
+        self.camera.start_recording(self.image_processor, format='yuv')
+
+        # start capturing. For each frame, the image processor's write() method is called
         try:
             while self.running:
-                self.camera.wait_recording(0.1)
-                # Get the current frame as a numpy array
-                frame = np.frombuffer(stream.getvalue(), dtype=np.uint8)
-
-                # Get the Y-component of the YUV stream
-                frame = frame.reshape((int(self.camera.resolution[1] * 1.5), self.camera.resolution[0]))
-                grayscale = frame[:self.camera.resolution[1], :self.camera.resolution[0]]
-
-                x, y = 100, 100 # Example pixel position
-                threshold = 128 # Example brightness threshold
-                brightness = grayscale[y, x]
-                print(brightness)
-
-                if brightness > threshold:
-                    pass
+                self.camera.wait_recording(1)
         finally:
             self.camera.stop_recording()
-            stream.close()
-            self.camera.close()
 
-    def capture(self, filename):
-        self.camera.capture(filename)
+    def capture(self) -> np.ndarray:
+        return self.image_processor.get_frame_safe()
 
     def close(self):
         self.camera.close()
--- tests/test_camera.py
+++ tests/test_camera.py
@@ -1,17 +1,37 @@
 import unittest
 import time
+from PIL import Image
 from ..camera import Camera
+from . import OUTPUT_DIRECTORY
 
 
-class Test_IPCConnector(unittest.TestCase):
-    def test_stream(self):
-        camera = Camera()
-        camera.start()
+class Test_Camera(unittest.TestCase):
+    def setUp(self):
+        self.camera = Camera()
+        self.camera.start()
 
-        time.sleep(2)
+    def tearDown(self):
+        self.camera.stop()
+        self.camera.close()
 
-        camera.stop()
+    def test_running(self):
+        # wait until the camera is running
+        while not self.camera.running:
+            time.sleep(0.1)
 
+    def test_capture(self):
+        # capture a frame
+        frame = self.camera.capture()
+
+        # check the frame size
+        self.assertEqual(frame.shape, (480, 640))
+
+        # make sure the frame is not empty
+        self.assertTrue(frame.any())
+
+        # save the frame to a file
+        image = Image.fromarray(frame)
+        image.save(OUTPUT_DIRECTORY / 'test_camera_capture.jpg')
 
 if __name__ == '__main__':
     unittest.main()
