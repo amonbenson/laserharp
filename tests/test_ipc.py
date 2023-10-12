4f0fd91cf0d7ab1790045592d123460f33fa2bdb
Amon Benson <amon.k.benson@campus.tu-berlin.de>
2023-10-12 21:40:12 +0200

    fixed test runs

--- tests/test_ipc.py
+++ tests/test_ipc.py
@@ -43,15 +43,15 @@ class Test_IPCConnector(unittest.TestCase):
 
     def test_send_midi(self):
         self.ipc.send(MidiEvent(1, mido.Message('note_on', note=60, velocity=64)))
-        self.assertEqual(self.serial.txdata, bytearray([0x01, 0x90, 60, 64]))
+        self.assertEqual(self.serial.txdata, bytearray([0x19, 0x90, 60, 64]))
 
     def test_read_midi(self):
-        self.serial.rxdata = bytearray([0x01, 0x90, 60, 64])
+        self.serial.rxdata = bytearray([0x38, 0x80, 60, 64])
 
         # validate packet midi conversion
         event = self.ipc.read()
-        self.assertEqual(event.cable_number, 1)
-        self.assertEqual(event.message, mido.Message('note_on', note=60, velocity=64))
+        self.assertEqual(event.cable_number, 3)
+        self.assertEqual(event.message, mido.Message('note_off', note=60, velocity=64))
 
 
 if __name__ == '__main__':
