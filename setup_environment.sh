e47c22e1329bbeb3269c8ac0218cfce3a41bb286
Amon Benson <amonkbenson@gmail.com>
2024-08-14 15:16:56 +0200

    updated venv configuration

--- setup_environment.sh
+++ setup_environment.sh
@@ -2,22 +2,17 @@
 set -eux
 
 DEBIAN_FRONTEND=noninteractive
-TZ=Europe/Berlin
 
 # Install the required packages
 sudo apt-get update
 sudo apt-get install -y python3 python3-venv python3-setuptools python3-pip python3-wheel
 
-# Install libcamera2 as a system package. This is the recommended way and avoids errors with missing headers.
-#sudo apt-get install -y python3-libcamera python3-kms++
-#sudo apt-get install -y qtbase5-dev qt5-qmake qtbase5-dev-tools python3-pyqt5 python3-prctl libatlas-base-dev ffmpeg libopenjp2-7 python3-pip libcap-dev
+# Install libcamera2 and opencv
+sudo apt-get install -y python3-picamera2 sudo python3-opencv --no-install-recommends
 
 # Create a virtual environment
-python3 -m venv --prompt laserharp .venv
+python3 -m venv --prompt laserharp --system-site-packages .venv
 source .venv/bin/activate
 
-# Install numpy first. This is part of the libcamera2 installation instructions.
-#pip3 install numpy --upgrade
-
 # Install python dependencies
-pip3 install --config-settings --confirm-license= --verbose -r requirements.txt
+pip3 install -r requirements.txt
