9f001f77eaca70fa78c835b1d1ba1807635ed4c5
Amon Benson <amonkbenson@gmail.com>
2024-09-09 11:16:24 +0200

    added usb renumeration

--- stm/USB_DEVICE/App/usb_device.c
+++ stm/USB_DEVICE/App/usb_device.c
@@ -1,21 +1,21 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : usb_device.c
-  * @version        : v2.0_Cube
-  * @brief          : This file implements the USB Device
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2024 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : usb_device.c
+ * @version        : v2.0_Cube
+ * @brief          : This file implements the USB Device
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2024 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 
 /* Includes ------------------------------------------------------------------*/
@@ -63,7 +63,20 @@ USBD_HandleTypeDef hUsbDeviceFS;
 void MX_USB_DEVICE_Init(void)
 {
   /* USER CODE BEGIN USB_DEVICE_Init_PreTreatment */
-
+    /*
+     * Force host to re-enumerate device (see https://stm32world.com/wiki/STM32_USB_Device_Renumeration)
+     * Assuming D+ is on PA12
+     */
+    GPIO_InitTypeDef GPIO_InitStruct = { 0 }; // All zeroed out
+    GPIO_InitStruct.Pin = GPIO_PIN_12; // Hardcoding this - PA12 is D+
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP; // Push-pull mode
+    GPIO_InitStruct.Pull = GPIO_PULLDOWN; // Resetting so pull low
+    GPIO_InitStruct.Speed = GPIO_SPEED_HIGH; // Really shouldn't matter in this case
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct); // Initialize with above settings
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12, GPIO_PIN_RESET); // Yank low
+    HAL_Delay(50); // Enough time for host to disconnect device
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12, GPIO_PIN_SET); // Back high - so host will enumerate
+    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_12); // Deinitialize the pin
   /* USER CODE END USB_DEVICE_Init_PreTreatment */
 
   /* Init Device Library, add supported class and start the library. */
