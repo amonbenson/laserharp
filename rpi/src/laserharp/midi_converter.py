b96432e62d9655f281de5a037aa544a13fce6cc1
Amon Benson <a.benson@komische-oper-berlin.de>
2024-09-18 17:15:10 +0200

    added midi converter

--- rpi/src/laserharp/midi_converter.py
+++ rpi/src/laserharp/midi_converter.py
@@ -9,6 +9,8 @@ from .din_midi import DinMidi
 
 class MidiConverter(Component):
     NUM_SECTIONS = 3
+    MAJOR_SCALE = [0, 2, 4, 5, 7, 9, 11]
+    STEP_NAMES = ["c", "d", "e", "f", "g", "a", "b"]
 
     def __init__(self, name: str, global_state: ReactiveDictNode, laser_array: LaserArray, din_midi: DinMidi):
         super().__init__(name, global_state)
@@ -16,10 +18,8 @@ class MidiConverter(Component):
         self._laser_array = laser_array
         self._din_midi = din_midi
 
-        self._num_lasers = len(self._laser_array)
-
         # setup an array of shape (num_sections, num_lasers) to keep track of which lasers are active
-        self.state["active"] = [[False] * self._num_lasers for _ in range(self.NUM_SECTIONS)]
+        self.state["active"] = [[False] * len(self._laser_array) for _ in range(self.NUM_SECTIONS)]
 
     def start(self):
         pass
@@ -34,11 +34,26 @@ class MidiConverter(Component):
 
         return start <= length < end
 
+    def _apply_scale(self, step: int) -> int:
+        step = step % len(self.MAJOR_SCALE)
+        step_name = self.STEP_NAMES[step]
+
+        # apply major scale
+        note = self.MAJOR_SCALE[step]
+
+        # apply alterations from the pedal positions
+        note += self.settings["pedal_position_" + step_name]
+
+        return note
+
     def process(self, interceptions: ImageProcessor.Result):
-        for x in range(self._num_lasers):
+        for x in range(len(self._laser_array)):
             active = interceptions.active[x]
             length = interceptions.length[x]
             _modulation = interceptions.modulation[x]
+
+            # use the diode index x as the step position and apply the current scale
+            note_offset = self._apply_scale(x)
 
             for y in range(self.NUM_SECTIONS):
                 note_active = active and self._is_in_section(y, length)
@@ -47,13 +62,16 @@ class MidiConverter(Component):
                 if self.state["active"][y][x] == note_active:
                     continue
 
-                note = self.config["root_note"] + y * self.config["section"]["octave_spacing"] * 12
+                # calculate the midi note
+                note = self.config["root_note"]
+                note += y * self.config["section"]["octave_spacing"] * 12
+                note += note_offset
 
                 # send the note on/off message
                 if note_active:
-                    pass
+                    self._din_midi.send(MidiEvent(0, "note_on", note=note, velocity=127))
                 else:
-                    pass
+                    self._din_midi.send(MidiEvent(0, "note_off", note=note))
 
                 # set the new state
                 self.state["active"][y][x] = note_active
