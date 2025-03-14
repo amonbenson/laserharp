e1eef65cb703be7726965c9c3dd160a21e8041a9
Amon Benson <amonkbenson@gmail.com>
2025-03-14 14:03:54 +0100

    added mqtt with scope, but not working atm

--- laserharp/common/mqtt.py
+++ laserharp/common/mqtt.py
@@ -1,6 +1,7 @@
 import os
 import trio
 import json
+import logging
 from collections import defaultdict
 from typing import Any, Optional
 from dataclasses import dataclass
@@ -10,6 +11,8 @@ from trio_paho_mqtt import AsyncClient
 
 MQTT_HOST = os.getenv("LH_MQTT_HOST", "127.0.0.1")
 MQTT_PORT = int(os.getenv("LH_MQTT_PORT", 1883))
+
+logger = logging.getLogger("lh:mqtt")
 
 
 @dataclass
@@ -30,23 +33,34 @@ class Subscription:
 class MQTTClient:
     def __init__(self):
         self._subscriptions: dict[str, list[Subscription]] = {}
+        self._running_event = trio.Event()
 
     async def _handle_messages(self, client: AsyncClient):
         try:
+            self._running_event.set()
+
             async for message in client.messages():
                 for topic_subs in self._subscriptions.values():
                     for sub in topic_subs:
                         if mqtt.topic_matches_sub(sub.topic, message.topic):
                             await sub.send_channel.send(message)
         except trio.Cancelled:
-            pass
+            self.disconnect()
 
     async def run(self, nursery: trio.Nursery):
+        logger.info("Connecting to broker...")
         self._sync_client = mqtt.Client()
         self._client = AsyncClient(self._sync_client, nursery)
         self._client.connect(MQTT_HOST, MQTT_PORT, keepalive=60)
 
         nursery.start_soon(self._handle_messages, self._client)
+
+    async def wait_connected(self):
+        await self._running_event.wait()
+
+    def disconnect(self):
+        logger.info("Disconnecting from broker...")
+        self._client.disconnect()
 
     def subscribe(self, topic, qos = 0, options = None, properties = None, raw=False):
         send_channel, receive_channel = trio.open_memory_channel(0)
@@ -78,8 +92,11 @@ class MQTTClient:
 
         self._client.publish(topic, payload)
 
+    async def __aenter__(self):
+        logger.info("Waiting for the subscription task to run...")
+        await self.wait_connected()
 
+    async def __aexit__(self, exc_type, exc, tb):
+        pass
 
-_global_mqtt_client = MQTTClient()
-def get_mqtt():
-    return _global_mqtt_client
+MQTT = MQTTClient()
--- laserharp/emulator/__main__.py
+++ laserharp/emulator/__main__.py
@@ -1,20 +1,25 @@
 import trio
-from ..common.mqtt import get_mqtt
+from ..common.mqtt import MQTT
 
 async def camera_stream():
-    await trio.sleep(0.5) # very ugly, use condition or another technique to wait for mqtt to be ready
-
     try:
         while True:
-            get_mqtt().publish("lh/emulator/camera/stream", "test")
+            MQTT.publish("lh/emulator/camera/stream", "test")
             await trio.sleep(0.5) # trio.sleep(1 / 50)
     except trio.Cancelled:
         pass
 
+async def start_components():
+    # make sure IO tasks are running until starting the main components
+    async with MQTT:
+        async with trio.open_nursery() as nursery:
+            nursery.start_soon(camera_stream)
+
 async def main():
     async with trio.open_nursery() as nursery:
-        nursery.start_soon(get_mqtt().run, nursery)
-        nursery.start_soon(camera_stream)
+        # start IO tasks first
+        nursery.start_soon(MQTT.run, nursery)
+        nursery.start_soon(start_components)
 
 if __name__ == "__main__":
     trio.run(main)
